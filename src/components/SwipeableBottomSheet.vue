89c11df664496c327f2176e089a5125d3e86d392
evd3v <evd3v@yandex.ru>
2020-08-26 18:11:24 +0300

    fix: fixed multiple swipeable

--- src/components/SwipeableBottomSheet.vue
+++ src/components/SwipeableBottomSheet.vue
@@ -124,14 +124,16 @@ export default {
 }
 
 .wrapper[data-open="true"] > .bg {
-  display: block;
-  transition: all .3s;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, .3);
+  & > .bg {
+    display: block;
+    transition: all .3s;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, .3);
+  }
 }
 
 .card {
